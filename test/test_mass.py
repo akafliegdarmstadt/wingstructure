b31aea7030475bdf57de440727d299b5b5232de0
Jonathan Hahn <Jonathan.Hahn@t-online.de>
2018-06-05 21:42:11 +0200

    add documentation and tests for Masspoint

---
+++ test/test_mass.py
@@ -0,0 +1,24 @@
+import pytest
+
+from wingstructure.mass import Masspoint
+
+
+def test_addition():
+    mp1 = Masspoint(1.0, (1.0, 0.0, 0.0))
+    mp2 = Masspoint(1.0, (-1.0, 0.0, 0.0))
+
+    mpsum = mp1+mp2
+
+    assert mpsum.mass == 2.0
+    assert mpsum.point[0] == 0.0
+
+
+def test_summation():
+    mp1 = Masspoint(1.0, (1.0, 0.0, 0.0))
+    mp2 = Masspoint(1.0, (-1.0, 0.0, 0.0))
+    mp3 = Masspoint(1.0, (0.0, 0.0, 0.0))
+
+    mpsum = sum((mp1, mp2, mp3))
+
+    assert mpsum.mass == 3.0
+    assert mpsum.point[0] == 0.0
--- wingstructure/mass.py
+++ wingstructure/mass.py
@@ -4,11 +4,21 @@
 class Masspoint(object):
     """Masspoints can be added to get the sum of masses and the common center of gravity"""
 
-    def __init__(self, mass, point):
+    def __init__(self, mass: float, point: tuple):
+        """
+        constructor for Masspoint
+        :param mass: masspoints mass
+        :param point: center of gravity of masspoint
+        """
         self.mass = mass
         self.point = point
 
     def __add__(self, othermasspoint):
+        """
+        Calculates sum of masses and common center of gravity
+        :param othermasspoint: another instance of Masspoint
+        :return: new instance of Masspoint, with sum of mass and common cg
+        """
         mass = self.mass + othermasspoint.mass
         cg = tuple(
             (self.mass * cg1 + othermasspoint.mass * cg2) \
@@ -17,6 +27,12 @@ class Masspoint(object):
         return Masspoint(mass, cg)
 
     def __radd__(self, othermasspoint):
+        """
+        Calculates sum of masses and common center of gravity
+        Is needed, when sum of Masspoints is calculated
+        :param othermasspoint: another instance of Masspoint
+        :return: new instance of Masspoint, with sum of mass and common cg
+        """
         if not isinstance(othermasspoint, Masspoint):
             return self
 
