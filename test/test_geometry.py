64aad03d286b83178653ab8d699bf90a2db72c22
Jonathan Hahn <Jonathan.Hahn@t-online.de>
2018-06-15 12:29:59 +0200

    Clean up examples

---
+++ test/test_geometry.py
@@ -0,0 +1,14 @@
+from wingstructure.geometry import Wing, Point
+import pytest
+
+# test sorting of sections
+
+def test_simple_wing_creation():
+
+    wing = Wing()
+
+    wing.add_section(Point(0,0,0), 1)
+    wing.add_section(Point(0,1,0), 1)
+
+    assert wing.area()==2.
+    assert wing.span_width()==2.
