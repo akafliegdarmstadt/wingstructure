9c74311808d5c03790fd20bf5b54886ba86c2cb2
Jonathan Hahn <Jonathan.Hahn@t-online.de>
2018-06-20 11:57:38 +0200

    fix bugs in multhopp

--- wingstructure/liftingline.py
+++ wingstructure/liftingline.py
@@ -5,8 +5,10 @@ A module providing multhop method for solving lifting line problem
 import numpy as np
 from numba import jit
 
+π = np.pi
+
 @jit
-def multhopp(αs, θs, chords, dcls):
+def multhopp(αs, θs, chords, b, dcls):
     """Calculates lift distribution with multhopp method.
     
     :param αs: angle of attack in radians at grid points
@@ -26,13 +28,22 @@ def multhopp(αs, θs, chords, dcls):
 
             # diagonal elements
             if v == n:
-                B[v, v] = (N_M+1)/(4*np.sin(theta_v))+2*b/(dcl_v*c)
+                B[v, v] = (M+1)/(4*np.sin(theta_v))+2*b/(dcl_v*c)
             # non diagonal elements
             else:
-                B[v, n] = - ((1-(-1.)**(v-n))/2*(np.sin(theta_n)/((N_M+1)*(np.cos(theta_n)-np.cos(theta_v))**2)))
+                B[v, n] = - ((1-(-1.)**(v-n))/2*(np.sin(theta_n)/((M+1)*(np.cos(theta_n)-np.cos(theta_v))**2)))
 
     # calculation of local circulation
     γs = np.dot(np.linalg.inv(B), αs)
 
     return γs
 
+def calculate_liftcoefficients(Θs, γs, chords, Λ, b, M):
+    """
+    Calculates lift coefficients with multhopp results
+    """
+
+    c_l = 2*b/(chords) * γs
+    C_L = π*Λ / (M+1) * np.sum(γs * np.sin(Θs))
+
+    return C_L, c_l
