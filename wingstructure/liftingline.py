914ee7e34dea53dca6825d2ddabab75f957d973f
Jonathan Hahn <Jonathan.Hahn@t-online.de>
2018-06-20 12:36:30 +0200

    fix minor bug in liftingline

--- wingstructure/liftingline.py
+++ wingstructure/liftingline.py
@@ -31,7 +31,8 @@ def multhopp(αs, θs, chords, b, dcls):
                 B[v, v] = (M+1)/(4*np.sin(theta_v))+2*b/(dcl_v*c)
             # non diagonal elements
             else:
-                B[v, n] = - ((1-(-1.)**(v-n))/2*(np.sin(theta_n)/((M+1)*(np.cos(theta_n)-np.cos(theta_v))**2)))
+                B[v, n] = - ((1-(-1.)**(v-n))/2*(np.sin(theta_n)/ \
+                            ((M+1)*(np.cos(theta_n)-np.cos(theta_v))**2)))
 
     # calculation of local circulation
     γs = np.dot(np.linalg.inv(B), αs)
@@ -43,7 +44,7 @@ def calculate_liftcoefficients(Θs, γs, chords, Λ, b, M):
     Calculates lift coefficients with multhopp results
     """
 
-    c_l = 2*b/(chords) * γs
+    c_l = 2*b/(np.array(chords)) * np.array(γs)
     C_L = π*Λ / (M+1) * np.sum(γs * np.sin(Θs))
 
     return C_L, c_l
