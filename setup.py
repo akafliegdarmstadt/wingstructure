cca9ddcd5934e90ccf711d44071a0c0fa1ba1f55
helo <Jonathan.Hahn@t-online.de>
2018-07-19 14:20:31 +0200

    remove testing from setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
 import sys, os
 from setuptools import setup, find_packages
 
-# taken from http://www.pydanny.com/python-dot-py-tricks.html
-if sys.argv[-1] == 'test':
-    test_requirements = [
-        'pytest',
-    ]
-    try:
-        modules = map(__import__, test_requirements)
-    except ImportError as e:
-        err_msg = e.message.replace("No module named ", "")
-        msg = "%s is not installed. Install your test requirments." % err_msg
-        raise ImportError(msg)
-    os.system('py.test')
-    sys.exit()
-# -------------------------------------------
-
 setup(name='wingstructure',
     version='0.0.1',
     description='',
