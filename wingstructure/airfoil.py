fee96288477d7daf3191837bbe749fec4ff4fe29
helo <Jonathan.Hahn@t-online.de>
2018-12-01 20:14:55 +0100

    delete airfoil.py

--- wingstructure/airfoil.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-class Airfoil(object):
-    def __init__(self, alpha0: float = 0, dif_ca_alpha: float = 2*np.pi, cm0: float = 0):
-        self.alpha0 = alpha0
-        self.dif_ca_alpha = dif_ca_alpha
-        self.cm0 = cm0
