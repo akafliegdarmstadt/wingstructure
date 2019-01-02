3f50f552f949ecbb348b57ffe3c83f6a021e8a77
helo <Jonathan.Hahn@t-online.de>
2019-01-02 17:54:46 +0100

    adds first version of internalreaction calculation

--- wingstructure/structure/__init__.py
+++ wingstructure/structure/__init__.py
@@ -1,4 +1,5 @@
-from . import section
+from . import section, internalreactions
 
 from .section import SectionBase, Layer, Reinforcement
 from .section import Display, ISpar, BoxSpar, MassAnalysis
+from .internalreactions import combine_loads, calc_moments
