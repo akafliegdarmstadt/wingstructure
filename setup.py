bdd1e278a4d4b14276642186631083f00624cd98
Jannik Birk <birk.jannik@gmail.com>
2019-01-11 13:20:13 +0100

    add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 import sys, os
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fg.read()
+
 setup(name='wingstructure',
     version='0.0.2',
     description='A library for structure calculations in airplane wings',
     url='https://github.com/akafliegdarmstadt/wingstructure',
     author='helo',
     author_email='kontakt@akaflieg.tu-darmstadt.de',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     packages=['wingstructure', 
               'wingstructure.data', 
