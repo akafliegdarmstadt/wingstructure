4fd7f3e9326e3bcf0fb098373e8fc072400a6203
helo <Jonathan.Hahn@t-online.de>
2019-01-15 22:17:54 +0100

    start documentation

--- wingstructure/structure/__init__.py
+++ wingstructure/structure/__init__.py
@@ -1,5 +1,5 @@
 from . import section, internalreactions, material
 
 from .section import SectionBase, Layer, Reinforcement
-from .section import Display, ISpar, BoxSpar, MassAnalysis
+from .section import Display, ISpar, BoxSpar, MassAnalysis, LineIdealisation
 from .internalreactions import combine_loads, calc_moments
