1d2607d9c9bce3832f9a372a57eb6dad6c0872fb
Jannik Birk <birk.jannik@gmail.com>
2019-01-23 23:51:36 +0100

    add two more tests

--- tests/test_liftingline.py
+++ tests/test_liftingline.py
@@ -23,6 +23,49 @@ def test_multhopp_schlichting():
     assert np.isclose(res[0][M//2:]/b*2, ηs_ref[:-1], atol=1e-4).all()
     assert np.isclose(res[1][M//2:], γs_ref[:-1], atol=1e-4).all()
 
+def test_multhopp_dcls_nan():
+    Λ = 6 # aspect ratio of wing
+    
+    b = 15 # m span width
+    cs = [b/Λ]*2 # depth of wing
+    ys = [0,b/2] # section positions
+
+    αs = [1]*2 # angle of attack
+    dcls = np.nan
+
+    # reference result
+    ηs_ref = [0,0.3827,0.7071,0.9239,1]
+    γs_ref = [0.4320,0.4192,0.3710,0.2485,0]
+
+    # coarse calculation
+    M = 7
+
+    res = multhopp(αs, cs, ys, dcls, M = M, mode='gamma')
+
+    assert np.isclose(res[0][M//2:]/b*2, ηs_ref[:-1], atol=1e-4).all()
+    assert np.isclose(res[1][M//2:], γs_ref[:-1], atol=1e-4).all()
+
+def test_multhopp_no_interp():
+    Λ = 6 # aspect ratio of wing
+    
+    b = 15 # m span width
+    cs = [b/Λ]*2 # depth of wing
+    ys = [0,b/2] # section positions
+
+    αs = [1]*2 # angle of attack
+    dcls = np.nan
+
+    # reference result
+    ηs_ref = [0,0.3827,0.7071,0.9239,1]
+    γs_ref = [0.4320,0.4192,0.3710,0.2485,0]
+
+    # coarse calculation
+    M = 7
+
+    res = multhopp(αs, cs, ys, dcls, M = M, mode='gamma', interp=False)
+
+    assert np.isclose(res[0][M//2:]/b*2, ηs_ref[:-1], atol=1e-4).all()
+    assert np.isclose(res[1][M//2:], γs_ref[:-1], atol=1e-4).all()
 
 def test_multhopp_coefficients():
     
