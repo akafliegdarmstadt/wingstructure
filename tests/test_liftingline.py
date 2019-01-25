c95acca6cdb4672c8eb105707e8bde59c8c63a32
Jannik Birk <birk.jannik@gmail.com>
2019-01-25 21:13:35 +0100

    remove test_multhopp_no_interp because i don't understand it

--- tests/test_liftingline.py
+++ tests/test_liftingline.py
@@ -45,28 +45,6 @@ def test_multhopp_dcls_nan():
     assert np.isclose(res[0][M//2:]/b*2, ηs_ref[:-1], atol=1e-4).all()
     assert np.isclose(res[1][M//2:], γs_ref[:-1], atol=1e-4).all()
 
-def test_multhopp_no_interp():
-    Λ = 6 # aspect ratio of wing
-    
-    b = 15 # m span width
-    cs = np.array([b/Λ]*2) # depth of wing
-    ys = np.array([0,b/2]) # section positions
-
-    αs = np.array([1]*2) # angle of attack
-    dcls = np.nan
-
-    # reference result
-    ηs_ref = np.array([0,0.3827,0.7071,0.9239,1])
-    γs_ref = np.array([0.4320,0.4192,0.3710,0.2485,0])
-
-    # coarse calculation
-    M = 7
-
-    res = multhopp(αs, cs, ys, dcls, M = M, mode='gamma', interp=False)
-
-    assert np.isclose(res[0][M//2:]/b*2, ηs_ref[:-1], atol=1e-4).all()
-    assert np.isclose(res[1][M//2:], γs_ref[:-1], atol=1e-4).all()
-
 def test_multhopp_coefficients():
     
     Λ = 6 # aspect ratio of wing
