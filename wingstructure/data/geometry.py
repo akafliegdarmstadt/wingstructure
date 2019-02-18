d7a31be6d8aae22c7e6b16d8080ee0040d169318
helo <Jonathan.Hahn@t-online.de>
2019-02-18 21:00:24 +0100

    adds within_flap function to Wing

--- wingstructure/data/geometry.py
+++ wingstructure/data/geometry.py
@@ -430,6 +430,11 @@ class Wing(_BaseWing):
             if flap.y_start <= span_pos <= flap.y_end:
                 return flap.depth_at(span_pos)/self.chord_at(span_pos)
         return 0.0
+
+    def within_flap(self, flapname:str, ys: np.ndarray)->np.ndarray:
+        flap = self.flaps[flapname]
+
+        return np.where((flap.y_start<=np.abs(ys)) & (np.abs(ys)<=flap.y_end), True, False)
         
     def set_airbrake(self, span_pos_start, span_pos_end):
         if span_pos_end > span_pos_start:
