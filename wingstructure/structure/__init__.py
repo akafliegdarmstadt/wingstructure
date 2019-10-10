5374320e6a3dfd85cd9dea0304eb710830090a8a
helo <Jonathan.Hahn@t-online.de>
2019-10-10 22:58:36 +0200

    import shapely dependend submodules only with shply installed

--- wingstructure/structure/__init__.py
+++ wingstructure/structure/__init__.py
@@ -1,5 +1,8 @@
+import importlib
 from . import section, stickmodel, beammechanics, material, polygon
 
-from .section import SectionBase, Layer, Reinforcement
 from .stickmodel import calc_lineloadresultants, solve_equilibrium
 from .polygon import calc_neutralcenter, calc_bendingstiffness
+
+if importlib.util.find_spec('shapely') is not None:
+    from .section import SectionBase, Layer, Reinforcement
