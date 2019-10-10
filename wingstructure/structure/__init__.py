e2e67799652a2ef790c6978c4f181be6b4bc8e34
helo <Jonathan.Hahn@t-online.de>
2019-10-10 23:01:36 +0200

    second try

--- wingstructure/structure/__init__.py
+++ wingstructure/structure/__init__.py
@@ -1,8 +1,9 @@
 import importlib
-from . import section, stickmodel, beammechanics, material, polygon
+from . import stickmodel, beammechanics, material, polygon
 
 from .stickmodel import calc_lineloadresultants, solve_equilibrium
 from .polygon import calc_neutralcenter, calc_bendingstiffness
 
 if importlib.util.find_spec('shapely') is not None:
+    from . import section
     from .section import SectionBase, Layer, Reinforcement
