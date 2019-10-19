fix sign bug

tests/test_stickmodell.py
@@ -180,7 +180,7 @@ def test_solve():
     ])
 
     # actual solving, setting forces and moments at node 2 to zero (prescribed)
-    sol = solve_equilibrium(nodes, forces, moments, prescribed={2:[0, 0, 0, 0, -1.0, 0]})
+    sol = solve_equilibrium(nodes, forces, moments, prescribed={2:[0, 0, 0, 0, 1.0, 0]})
 
     # check with manual calculation
     assert np.isclose(sol, [

wingstructure/structure/stickmodel.py
@@ -298,7 +298,7 @@ def solve_equilibrium(nodes, forces=np.zeros((1,7)), moments=np.zeros((1,4)), pr
             if prescribed_value is None:
                 continue
             A[6*(n-1) + i][6*node + i] = 1
-            b[6*(n-1) + i] = prescribed_value
+            b[6*(n-1) + i] = -prescribed_value
 
     # external loads
     for i in range(n-1):